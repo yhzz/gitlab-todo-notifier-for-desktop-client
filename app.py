8a01990e061a5e4965529bb5862a9c3baab41f2b
Yohei Zaiznen <20seikisyounen@gmail.com>
2021-03-30 22:22:04 +0900

    Give up

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
 from plyer import notification
+import time
+import sys
+from pathlib import Path
+import servicemanager
+import socket
+import sys
+import win32event
+import win32service
+import win32serviceutil
 
-notification.notify(
-    title='通知だよ',
-    message='これはメッセージだよ',
-    app_name='アプリ名だよ',
-    app_icon='./notifications-outline.ico'
-)
+
+# def resource_path(relative_path):
+#     base_path = getattr(sys, '_MEIPASS', str(Path(__file__).absolute().parent))
+#     return str(Path(base_path).joinpath(relative_path))
+
+
+class TestService(win32serviceutil.ServiceFramework):
+    _svc_name_ = "TestService"
+    _svc_display_name_ = "Test Service"
+    _svc_description_ = "My service description"
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        rc = None
+        while rc != win32event.WAIT_OBJECT_0:
+            with open('C:\\TestService.log', 'a') as f:
+                f.write('test service running...\n')
+            
+            rc = win32event.WaitForSingleObject(self.hWaitStop, 5000)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(TestService)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(TestService)
